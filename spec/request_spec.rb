a471ff0e0db33bfb4adb06b4d1a07eca97d24e9b
Colin Ewen <colin@draecas.com>
2014-08-06 15:07:10 -0400

    0.2.0 major breaking changes oh no

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -6,15 +6,15 @@ describe Cortex::Request do
   context 'with a cortex error response' do
     it 'should return the error object' do
       body = {'error' => 'Validation error or something'}
-      response = OpenStruct.new(:status => 422, :body => body)
-      client.parse_response(response).should == body
+      response = OpenStruct.new(:status => 422, :body => body, :headers => { :whatever => "Whatever"})
+      client.parse_response(response).to_h.should == { :body => body, :headers => {}}
     end
   end
 
   context 'with a non-cortex error response' do
     it 'should return a wrapped response' do
       body = 'Catastrophic error'
-      response = OpenStruct.new(:status => 500, :body => body)
+      response = OpenStruct.new(:status => 500, :body => body, :headers => { :whatever => "Whatever" })
       parsed = client.parse_response(response)
       parsed.error.should == body
       parsed.status.should == 500
@@ -25,8 +25,8 @@ describe Cortex::Request do
   context 'with a successful response' do
     it 'should return the parsed body' do
       body = {:id => 1, title: 'A post'}
-      response = OpenStruct.new(:status => 200, :body => body)
-      client.parse_response(response).should == body
+      response = OpenStruct.new(:status => 200, :body => body, :headers => { :whatever => "Whatever" })
+      client.parse_response(response).to_h.should == { :body => body, :headers => {}}
     end
   end
 end
