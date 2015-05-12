c7dc2e98a822da1f847c1bf96baeca5162b473d2
Colin Ewen <colin@draecas.com>
2015-05-12 14:23:19 -0400

    Posts spec + fix constant names

--- lib/cortex/client/resources/posts.rb
+++ lib/cortex/client/resources/posts.rb
@@ -7,11 +7,11 @@ module Cortex
         end
 
         def index(criteria=nil)
-          v1.get(APIs::V1::Endpoints::SINGLE_ITEM, {type: 'media', id: nil}, criteria)
+          v1.get(APIs::V1::Endpoints::SINGLE_ITEM % {type: 'posts', id: nil}, criteria)
         end
 
         def get(id)
-          v1.get(APIs::V1::Endpoints::SINGLE_ITEM % {type: 'media', id: id})
+          v1.get(APIs::V1::Endpoints::SINGLE_ITEM % {type: 'posts', id: id})
         end
 
         def get_published(id)
@@ -23,23 +23,23 @@ module Cortex
         end
 
         def filters
-          v1.get(APIs::V1::Endpoints::Filters)
+          v1.get(APIs::V1::Endpoints::FILTERS)
         end
 
         def authors
-          v1.get(APIs::V1::Endpoints::Authors)
+          v1.get(APIs::V1::Endpoints::AUTHORS)
         end
 
         def create(body)
-          v1.post(APIs::V1::Endpoints::SINGLE_ITEM % {type: 'media', id: nil}, body)
+          v1.post(APIs::V1::Endpoints::SINGLE_ITEM % {type: 'posts', id: nil}, body)
         end
 
         def save(id, body)
-          v1.put(APIs::V1::Endpoints::SINGLE_ITEM % {type: 'media', id: id}, body)
+          v1.put(APIs::V1::Endpoints::SINGLE_ITEM % {type: 'posts', id: id}, body)
         end
 
         def delete(id)
-          v1.delete(APIs::V1::Endpoints::SINGLE_ITEM % {type: 'media', id: id})
+          v1.delete(APIs::V1::Endpoints::SINGLE_ITEM % {type: 'posts', id: id})
         end
       end
     end
