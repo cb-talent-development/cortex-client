a25d90c935cf1805ae715816d5ab9b00778c82f1
Alex Tharp <toastercup@gmail.com>
2016-04-08 19:41:00 -0500

    Update various dependencies

--- lib/cortex/webpages.rb
+++ lib/cortex/webpages.rb
@@ -1,7 +1,7 @@
 module Cortex
   class Webpages < Cortex::Resource
     def get_feed(id)
-      client.get("/webpages/feed", {:url => id})
+      client.get('/webpages/feed', {:url => id})
     end
   end
 end
